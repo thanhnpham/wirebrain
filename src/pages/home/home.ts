cbae25c8ad4eedefc4b1cc1404e390430b342d11
Thanh Pham <thanh.pham@innovexdownhole.com>
2018-04-24 15:28:52 -0500

    'icons'

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,12 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  menuData = [
+    { title: 'Our Menu', pic:'assets/img/soup1.jpg', pushPage: 'MenuPage'},
+    { title: 'Account', pic:'assets/img/coffee-people3.jpg', pushPage: 'AccountPage'},
+    { title: 'About Us', pic:'assets/img/coffee6.jpg', pushPage: 'AboutPage'},
+    { title: 'Locations', pic:'assets/img/cafe2.jpg', pushPage: 'LocationsPage'}    
+  ]
   constructor(public navCtrl: NavController) {
 
   }
