b1dced5cc3d76dd3713e64b9913db5e0086aadfd
Thanh Pham <thanh.pham@innovexdownhole.com>
2018-04-30 07:16:29 -0500

    fix error

--- src/pages/checkout/checkout.ts
+++ src/pages/checkout/checkout.ts
@@ -111,4 +111,46 @@ export class CheckoutPage implements OnInit {
     this.discountUsed = false;
     this.discount = '';
   }
+
+  purchase() {
+    if (this.discountUsed) {
+      let tmpId = this.discount.rewardId;
+      let tmp = this.rewardsList.map(x => x.rewardId).indexOf(tmpId);
+      if (tmp > -1) {
+        this.rewardsList.splice(tmp, 1);
+      } 
+      this.userSvc.storageControl('set', `${this.customer}-rewards`)
+        .then(results => console.log('Saved:', results));
+      this.payCart(this.discountTotal);
+      this.cartSvc.emptyCart();
+      this.userSvc.displayAlert('Thank you', `Your order for ${this.discountTotal} has been paid`);
+      this.navCtrl.push(HomePage);
+    }
+    else {
+      this.payCart(this.orderTotal);
+      this.cartSvc.emptyCart();
+      this.userSvc.displayAlert('Thank you', `Your order for ${this.discountTotal} has been paid`);
+      this.navCtrl.push(HomePage);
+    }
+  }
+  payCart(amt) {
+    this.payPal.init({
+      PayPalEnvironmentProduction: '',
+      PayPalEnvironmentSandbox: ''      
+    }).then(() => {
+      this.payPal.prepareToRender('PayPalEnvironmentSandbox', new PayPalConfiguration({        
+      })).then(() => {
+        let payment = new PayPalPayment(amt, 'USD', 'Description', 'sale');
+        this.payPal.renderSinglePaymentUI(payment).then((res) => {
+          console.log('Result from Paypal', res);
+        }, (err) => {
+          console.log('Error: ', err) 
+        });
+      },(conf) => {
+        console.log('Configuration Error: ', conf)
+      });
+    }, (init) => {
+      console.log('Init Error: ', init)
+    });
+  }
 }
